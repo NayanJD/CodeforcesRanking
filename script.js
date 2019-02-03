1f7f1f8a690b1574dde73db85358e39f7b0cfe81
Mohit Kumar Bharadwaj <mohit@Mohits-MacBook-Air.local>
2019-02-03 14:19:07 +0530

    Third Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function statLoader() {
+//   // fetch("https://codeforces.com/api/user.ratedList?activeOnly=true").then(
+//   //   populate
+//   // );
+//   console.log("Entered");
+//   setTimeout(1000);
+//   populate("<tr class=\"tabrow\"\>
+//   <td class=\"ranking\">1</td\>
+//   <td class=\"player\">tourist</td\>
+//   <td class=\"rating\">3200</td\>
+// </tr>");
+// }
+
+// function populate(result) {
+//   document.querySelector(".tab").insertAdjacentHTML('beforeend',result);
+// }
+
+// statLoader();
+
+console.log("log");
